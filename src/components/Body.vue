f9d4cfda61b2f8bf7955c179f42a9a6dfbde263e
Zeyu-Li <zeyu.li23@gmail.com>
2021-10-12 19:56:35 -0600

    converted to vue 🥳

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -1,71 +1,71 @@
 <template>
-<div>
-  <div style="height: 50%; width: 100%; display: flex; justify-content: center">
-    <textarea
-      style="width: 100%; height: 50%; text-align: left"
-      rows="14"
-      placeholder="Input LaTeX here"
-      class="form-control"
-      v-model="inputString"
-    ></textarea>
-  </div>
-
-  <div style="height: 50%" class="outputSection">
-    <hr />
-    <div style="display: flex; justify-content: space-between">
-      <p>Output Text:</p>
-      <button
-        type="button"
-        title="Copy output to clipboard"
-        class="btn btn-primary copyButton"
-        @click="copyButtonClick()"
-      >
-        Copy Output to Clipboard 📋
-      </button>
+  <div>
+    <div
+      style="height: 50%; width: 100%; display: flex; justify-content: center"
+    >
+      <textarea
+        style="width: 100%; height: 50%; text-align: left"
+        rows="14"
+        placeholder="Input LaTeX here"
+        class="form-control"
+        v-model="inputString"
+        @input="changeTextInput()"
+      ></textarea>
     </div>
-    <code id="output">{{ outputString }}</code>
-  </div>
+
+    <div style="height: 50%" class="outputSection">
+      <hr />
+      <div style="display: flex; justify-content: space-between">
+        <p>Output Text:</p>
+        <button
+          type="button"
+          title="Copy output to clipboard"
+          class="btn btn-primary copyButton"
+          @click="copyButtonClick()"
+        >
+          Copy Output to Clipboard 📋
+        </button>
+      </div>
+      <code id="output">{{ outputString }}</code>
+    </div>
   </div>
 </template>
 
 <script>
-// const nonsense = "#*%@^";
-let inputString = "";
-let outputString = "sad";
-
-// area.addEventListener('input', () => {
-//     outputString = ""
-//     const newInput = area.value.replaceAll("\\$", nonsense)
-
-//     let start = true
-//     for (let i = 0; newInput && i < newInput.length; i++) {
-//         if (newInput[i] === "$") {
-//             outputString += start ? "\\(" : "\\)"
-//             start = !start
-//         } else {
-//             outputString += newInput[i]
-//         }
-//     }
-//     // convert nonsense back to regular dollar sign
-//     output.innerText = outputString.replaceAll(nonsense, "$")
-// }, false)
-
-const copyButtonClick = () => {
-  navigator.clipboard.writeText(outputString);
-};
+// let inputString = "";
+// let outputString = "";
 
 export default {
   name: "Body",
-  methods: {
-    copyButtonClick,
-  },
-  data: () => {
+  data() {
     return {
-      
-    inputString,
-    outputString
-    }
-  }
+      inputString: "",
+      outputString: "",
+    };
+  },
+  methods: {
+    copyButtonClick() {
+      navigator.clipboard.writeText(this.outputString);
+    },
+    changeTextInput() {
+      // console.log(this.inputString);
+      const nonsense = "#*%@^";
+      let newOutputString = "";
+      const newInput = this.inputString.replaceAll("\\$", nonsense);
+
+      let start = true;
+      for (let i = 0; newInput && i < newInput.length; i++) {
+        if (newInput[i] === "$") {
+          newOutputString += start ? "\\(" : "\\)";
+          start = !start;
+        } else {
+          newOutputString += newInput[i];
+        }
+      }
+      // convert nonsense back to regular dollar sign
+      this.outputString = newOutputString.replaceAll(nonsense, "$");
+    },
+  },
 };
 </script>
 
